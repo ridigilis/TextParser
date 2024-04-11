cb1807a5c8d6b5c8f621f884621f6806272f5f61
ridigilis <ridigilis@ridigilis.dev>
2024-04-11 12:15:14 -0500

    add flags options and config

--- TextParser/App.swift
+++ TextParser/App.swift
@@ -14,29 +14,76 @@ struct App: ParsableCommand {
     @Argument(help: "The text you want to analyze")
     var input: [String]
     
+    @Flag(name: .shortAndLong, help: "Show detected language.")
+    var detectLanguage = false
+    
+    @Flag(name: .shortAndLong, help: "Prints how positive or negative the input is.")
+    var sentimentAnalysis = false
+    
+    @Flag(name: .shortAndLong, help: "Shows the stem form of each word in the input.")
+    var lemmatize = false
+    
+    @Flag(name: .shortAndLong, help: "Prints alternative words for each word in the input.")
+    var alternatives = false
+    
+    @Flag(name: .shortAndLong, help: "Prints names of people, places, and organizations in the input.")
+    var names = false
+    
+    @Option(name: .shortAndLong, help: "The maximum number of alternatives to suggest")
+    var maximumAlternatives = 10
+    
+    static var configuration: CommandConfiguration {
+        CommandConfiguration(commandName: "analyze", abstract: "Analyzes input text using a range of natural language approaches.")
+    }
+    
     mutating func run() {
-        let text = input.joined(separator: " ")
-        
-        let language = NLLanguageRecognizer.dominantLanguage(for: text) ?? .undetermined
-        print()
-        print("Detected language: \(language.rawValue)")
-        
-        let entities = entities(for: text)
-        print()
-        print("Found the following entities:")
-        for entity in entities {
-            print("\t", entity)
+        if detectLanguage == false && sentimentAnalysis == false && lemmatize == false && alternatives == false && names == false {
+            detectLanguage = true
+            sentimentAnalysis = true
+            lemmatize = true
+            alternatives = true
+            names = true
         }
         
-        print()
-        let sentiment = sentiment(for: text)
-        print("Sentiment analysis: \(sentiment)")
-        print()
-        print("Found the following alternatives:")
-        let lemma = lemmatize(string: text)
-        for word in lemma {
-            let embeddings = embeddings(for: word)
-            print("\t\(word): ", embeddings.formatted(.list(type: .and)))
+        let text = input.joined(separator: " ")
+        
+        if detectLanguage {
+            let language = NLLanguageRecognizer.dominantLanguage(for: text) ?? .undetermined
+            print()
+            print("Detected language: \(language.rawValue)")
+        }
+        
+        if names {
+            let entities = entities(for: text)
+            print()
+            print("Found the following entities:")
+            for entity in entities {
+                print("\t", entity)
+            }
+        }
+        
+        if sentimentAnalysis {
+            print()
+            let sentiment = sentiment(for: text)
+            print("Sentiment analysis: \(sentiment)")
+        }
+        
+        lazy var lemma = lemmatize(string: text)
+        
+        if lemmatize {
+            print()
+            print("Found the following lemma:")
+            print("\t", lemma.formatted(.list(type: .and)))
+        }
+        
+        if alternatives {
+            print()
+            print("Found the following alternatives:")
+            
+            for word in lemma {
+                let embeddings = embeddings(for: word)
+                print("\t\(word): ", embeddings.formatted(.list(type: .and)))
+            }
         }
     }
     
@@ -52,7 +99,7 @@ struct App: ParsableCommand {
     func embeddings(for word: String) -> [String] {
         var results = [String]()
         if let embedding = NLEmbedding.wordEmbedding(for: .english) {
-            let similarWords = embedding.neighbors(for: word, maximumCount: 10)
+            let similarWords = embedding.neighbors(for: word, maximumCount: maximumAlternatives)
             for word in similarWords {
                 results.append("\(word.0) has a distance of \(word.1)")
             }
